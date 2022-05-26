e91026796905979dac3609590f4d798d10c23826
Rishad Alam <sazzadullalamrishad@gmail.com>
2022-05-26 19:23:38 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    // let nav = $('.nav-bar');
+    // let navTop =nav.offset().top;
+    // alert(navTop);
+    
+    let nav = $('.nav-bar');
+    let navTop =nav.offset().top;
+    alert(navTop);
+
+});
